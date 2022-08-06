d8058cdc85382698eb610061846c0e0535feb146
MateoPeri <mateoperirufo@gmail.com>
2022-08-05 21:11:30 -0300

    primer base heroku

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'dcpf5qd3fhfgho',
+      user:     'japimxmroxyqfj',
+      password: 'f6ddbe474fe2d4d079f79eaf4604fab2f9296672ed6436adf0bb6e0c76d49bd1'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
